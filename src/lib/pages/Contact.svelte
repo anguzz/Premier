ae6fb0bf8bd836b93f5ca0a171b09e4f24eee289
Angel S <santoyox714@gmail.com>
2023-02-09 19:37:27 -0800

    update

--- src/lib/pages/Contact.svelte
+++ src/lib/pages/Contact.svelte
@@ -9,24 +9,9 @@
   import {Swipeable, Preload, Screen, Controls} from 'thumb-ui'
   let current=1
 
-  let images = [
-    {url: 'https://i.imgur.com/WSNVjAp.jpg', name: 'Lamborghini Veneno'},
-    {url: 'https://i.imgur.com/ktLr47i.jpg', name: 'Zenvo TS1'},
-    {url: 'https://i.imgur.com/IBPaYOH.jpg', name: 'McLaren P1 LM'},
-    {url: 'https://i.imgur.com/E97i8c8.jpg', name: 'LaFerrari FXX K'},
-    {url: 'https://i.imgur.com/eurkXGH.jpg', name: 'Bugatti Chiron'},
-    {url: 'https://i.imgur.com/1BdAJno.jpg', name: 'Lamborghini Centenario'},
-    {url: 'https://i.imgur.com/0AZOG1D.jpg', name: 'Aston Martin One-77'},
-    {url: 'https://i.imgur.com/qlgU0n9.jpg', name: 'Pagani Huayra'},
-    {url: 'https://i.imgur.com/Qol6aNw.jpg', name: 'W Motors Lykan Hypersport'}
-  ]
-  //import { images } from '../utils/images.js';
-  let showThumbs=false
-  let showCaptions=false
-  let showIndicators=false
-  let slideControls=false
-  let color = "text-grey-200";
-  let hover = "sm:hover:text-primary-500";
+ 
+  import { images } from '../utils/images.js';
+
 </script>
 <Anchor id="contact" />
 
--- src/lib/utils/images.js
+++ src/lib/utils/images.js
@@ -1,11 +1,20 @@
 export let images = [
-    {url: 'https://i.imgur.com/WSNVjAp.jpg', name: 'Lamborghini Veneno'},
-    {url: 'https://i.imgur.com/ktLr47i.jpg', name: 'Zenvo TS1'},
-    {url: 'https://i.imgur.com/IBPaYOH.jpg', name: 'McLaren P1 LM'},
-    {url: 'https://i.imgur.com/E97i8c8.jpg', name: 'LaFerrari FXX K'},
-    {url: 'https://i.imgur.com/eurkXGH.jpg', name: 'Bugatti Chiron'},
-    {url: 'https://i.imgur.com/1BdAJno.jpg', name: 'Lamborghini Centenario'},
-    {url: 'https://i.imgur.com/0AZOG1D.jpg', name: 'Aston Martin One-77'},
-    {url: 'https://i.imgur.com/qlgU0n9.jpg', name: 'Pagani Huayra'},
-    {url: 'https://i.imgur.com/Qol6aNw.jpg', name: 'W Motors Lykan Hypersport'}
+    {url: 'https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2010/2/8/0/istock-2017881_patio-hardscaping-gray-pavers_s4x3.jpg.rend.hgtvcom.1280.960.suffix/1400946680511.jpeg',
+	 name: 'Patio1'
+	},
+	
+	{url: 'https://media.architecturaldigest.com/photos/62f14b9a3061c43602fd03ec/master/pass/Skornicka-298%20Seadrift-Adam%20Potts%20WEB25%20copy.jpg',
+	 name: 'patio2'
+	},
+    
+	{url: 'https://luxurypools.com/wp-content/uploads/2018/05/Poolscape-with-three-separate-bistro-patio-areas_Rugged-Class-Waterfalls-Pools-488x325.jpg', 
+	name: 'Patio3'
+    },
+	{url: 'https://st.hzcdn.com/simgs/pictures/patios/townsend-yardscapes-northwest-img~c901dca407b6331c_14-3892-1-10f13ff.jpg', 
+	name: 'Patio3'
+    },
+
+
   ]
+
+  
